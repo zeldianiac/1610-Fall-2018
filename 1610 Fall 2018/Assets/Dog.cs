4be7653acfea96dd7fd27beb9597c8c2f3ed6aeb
Colin Richins <zeldianiac@gmail.com>
2018-09-10 15:33:34 -0600

    Added Basic Scripting

---
+++ 1610 Fall 2018/Assets/Dog.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : Animal {
+
+	// Use this for initialization
+	void Awake ()
+	{
+		print("I am a dog.");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

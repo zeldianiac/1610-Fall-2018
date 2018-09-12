e1c68ad5779dd0c0b83fd0d9ead0b9b7685407cc
Colin Richins <zeldianiac@gmail.com>
2018-09-12 15:29:36 -0600

    Floats and Components
    Added floats and component stuffs

---
+++ 1610 Fall 2018/Assets/Move.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	public Vector3 RotateSpeed;
+
+	public CharacterController Character;
+	
+	public float Speed;
+
+	public Vector3 MoveSpeed;
+	// Update is called once per frame
+	void Update ()
+	{
+		MoveSpeed.x = Speed*Input.GetAxis("Horizontal");
+		MoveSpeed *= Time.deltaTime;
+		
+		Character.Move(MoveSpeed);
+
+
+
+
+	}
+}

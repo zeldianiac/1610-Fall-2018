8aaa7c820737ae6d75056118718c20e8b9ea7e0e
Colin Richins <zeldianiac@gmail.com>
2018-09-17 15:45:33 -0600

    Added Variables

---
+++ 1610 Fall 2018/Assets/AddMoney.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AddMoney : MonoBehaviour
+{
+	public int Money = 10;
+	
+	// Use this for initialization
+	public void AddMoreMoney (int money)
+	{
+		Money += money;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ 1610 Fall 2018/Assets/Variables.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+	public bool IsaWizard;
+	
+	public int AmmoCount = 100;
+	public int InventoryCount;
+
+	public float Health = 1.0f;
+	public float Money;
+
+	public string PlayerName;
+	public string Team = "Wolves";
+
+	public Vector3 Location;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		print(PlayerName);
+		print("has");
+		print("$" + Money);
+		if (IsaWizard)
+		{
+			print(PlayerName + "Is a Wizard");
+		}
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		transform.position = Location;
+	}
+}

72675938249f6328be3ff9d50c411ae39ca18790
Colin Richins <zeldianiac@gmail.com>
2018-10-03 15:43:44 -0600

    Player Controller
    Made a cube that can move and jump

---
+++ 1610 Fall 2018/Assets/Scripts/CharacterMover.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMover : MonoBehaviour
+{
+
+	public CharacterController Controller;
+	public float Gravity = 9.81f;
+	public float MoveSpeed = 3.0f;
+	public float JumpSpeed = 3.0f;
+	private Vector3 position;
+	private Vector3 rotation;
+
+	// Use this for initialization
+	void Start ()
+	{
+		Controller = GetComponent<CharacterController>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Controller.isGrounded)
+		{
+			position.Set(0, 0, MoveSpeed*Input.GetAxis("Vertical"));
+			rotation.Set(0, Input.GetAxis("Horizontal"), 0);
+			transform.Rotate(rotation);
+			position = transform.TransformDirection(position);
+			
+			
+			if (Input.GetButton("Jump"))
+			{
+				position.y = JumpSpeed;
+			}
+
+			;
+		}
+		position.y -= Gravity * Time.deltaTime;
+		Controller.Move(position * Time.deltaTime);
+	}
+}

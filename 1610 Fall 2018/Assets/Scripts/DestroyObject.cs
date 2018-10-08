6d7df587c52a8c0f9e239b570a2da1db1be9c3ef
Unknown <zeldianiac@gmail.com>
2018-10-08 15:27:35 -0600

    Instancing and Destroy Object
    We shot some stuff. Done blowed it up we did

---
+++ 1610 Fall 2018/Assets/Scripts/DestroyObject.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject: MonoBehaviour {
+
+	// Use this for initialization
+	IEnumerator Start () {
+		yield return new WaitForSeconds(2);
+		Destroy(gameObject);
+	}
+
+	private void OnCollisionEnter(Collision other)
+	{
+		Destroy(gameObject);
+		Destroy(other.gameObject);
+	}
+}
---
+++ 1610 Fall 2018/Assets/Scripts/Instancing.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instancing : MonoBehaviour
+{
+
+	public GameObject Instance;
+
+	// Use this for initialization
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.X))
+		{
+			Instantiate(Instance, transform.position, transform.rotation);
+
+		}
+
+
+	}
+}
+	

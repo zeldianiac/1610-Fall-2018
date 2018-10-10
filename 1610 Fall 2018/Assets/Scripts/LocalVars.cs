8aa18ee7f4f51b8e47549c766eda1603a463b54e
Unknown <zeldianiac@gmail.com>
2018-10-10 15:26:50 -0600

    LocalVariables and Privates
    My Local Variable didn't work, but that may be due to my older Unity at home

---
+++ 1610 Fall 2018/Assets/Scripts/LocalVars.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LocalVars : MonoBehaviour
+{
+    public string Name;
+
+    public void Call()
+    {
+        Name = "Bob";
+        print(Name);
+    }
+
+    public void Call(string newName)
+    {
+        print(newName);
+        print(Name);
+    }
+}
---
+++ 1610 Fall 2018/Assets/Scripts/WhatCanSeeIt.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+
+public class WhatCanSeeIt: ScriptableObject
+{
+    private float health;
+    protected float PowerLevel;
+    public float EnergyLevel;
+
+    private void AddHealth()
+    {
+        health += 100;
+    }
+
+    protected void AddPower()
+    {
+        AddHealth();
+    }
+
+    public void AddEnergy()
+    {
+        
+    }
+}
+
+public class ICanSeeIt: MonoBehaviour
+{
+    private WhatCanSeeIt newObject;
+
+    private void Start()
+    {
+        newObject.EnergyLevel = 100;
+        newObject.AddEnergy();
+    }
+}
+
+public class Child : WhatCanSeeIt
+{
+    void CallThings()
+    {
+        PowerLevel = 200;
+        EnergyLevel = 300;
+        AddPower();
+        AddEnergy();
+        
+    }
+}

96280efa4ad3b09a35ed49fe4122d99fd6d390a3
Unknown <zeldianiac@gmail.com>
2018-10-15 15:51:53 -0600

    Collections and Such
    the stream acted strangely at the end. Ask Romwell about it

---
+++ 1610 Fall 2018/Assets/Scripts/AddToCollection.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class AddToCollection : ScriptableObject
+{
+
+    public ObjectCollection Collection;
+
+    public void Call(ObjectCollection obj)
+    {
+        Collection.ObjectList.Add(obj);
+    }
+}

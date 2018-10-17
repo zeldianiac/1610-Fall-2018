19263fa04d14780cf6fca91b99eecbfec3810452
Unknown <zeldianiac@gmail.com>
2018-10-17 15:45:11 -0600

    Health Bar and update health

---
+++ 1610 Fall 2018/Assets/Scripts/HealthBar/UpdatedImageValue.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdatedImageValue : MonoBehaviour
+{
+
+	public FloatData Data;
+	private Image image;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		image = GetComponent<Image>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		image.fillAmount = Data.Value;
+	}
+}
---
+++ 1610 Fall 2018/Assets/Scripts/HealthBar/ValueChange.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu]
+public class ValueChange : ScriptableObject
+{
+
+	public FloatData Data;
+
+	public void AddValue (FloatData data)
+	{
+		Data.Value += data.Value;
+	}
+
+	public void SubtractValue (FloatData data)
+	{
+		Data.Value -= data.Value;
+	}
+}

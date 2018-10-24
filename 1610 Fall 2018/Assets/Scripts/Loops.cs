eaa67e96adbd2bebca0c02ae0a8d7b3b2ded72f7
Colin Richins <zeldianiac@gmail.com>
2018-10-24 15:05:48 -0600

    Loops
    Need to create some loops at home

---
+++ 1610 Fall 2018/Assets/Scripts/Loops.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loops : MonoBehaviour
+{
+
+	public string[] Names;
+	public GameObject[] Things;
+
+	// Use this for initialization
+	void Start ()
+	{
+		for (int i = 0; i < Names.Length; i++)
+		{
+			print(Names[i]);
+		}
+
+		foreach (var thing in Things)
+		{
+			print(thing);
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

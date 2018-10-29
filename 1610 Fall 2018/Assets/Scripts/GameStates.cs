fe40848c8a06c33fc7e80a6f9a286b0b1ece9695
Colin Richins <zeldianiac@gmail.com>
2018-10-29 15:28:51 -0600

    Change States and King of the Hill

---
+++ 1610 Fall 2018/Assets/Scripts/GameStates.cs
@@ -0,0 +1,17 @@
+﻿
+public class GameStates
+{
+    public enum States
+    {
+        Starting,
+        Loading,
+        Playing,
+        Dying,
+        Ending,
+        Team1Score,
+        Team2Score
+        
+    }
+
+    public States State;
+}
---
+++ 1610 Fall 2018/Assets/Scripts/SwitchStatements.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SwitchStatements : MonoBehaviour
+{
+
+	public GameStates.States GameState;
+
+	public UnityEvent Starting, Loading, Playing, Ending, Dying, Team1Score, Team2Score;
+
+	public void ChangeState(GameStates.States state)
+	{
+		GameState = state;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch (GameState)
+		{
+				case GameStates.States.Starting:
+					print("We are starting");
+					break;
+				
+				case GameStates.States.Playing:
+					print("We are playing the game.");
+					break;
+				case GameStates.States.Ending:
+					print("We are ending the game. You probably lost didnt' you?");
+					break;
+				case GameStates.States.Loading:
+					print("Loading...");
+					break;
+				case GameStates.States.Dying:
+					print("You are dead. Donezo. Fine. Screwed. Capped.");
+					break;
+				
+				case GameStates.States.Team1Score:
+					break;
+				case GameStates.States.Team2Score:
+					break;
+		}
+	}
+}

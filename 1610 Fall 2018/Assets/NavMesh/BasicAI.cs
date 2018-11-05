b879391272819c0d5d5cf94435f9aaa65a891fdb
Colin Richins <zeldianiac@gmail.com>
2018-11-05 15:10:05 -0700

    NavMesh

---
+++ 1610 Fall 2018/Assets/NavMesh/BasicAI.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+
+public class BasicAI : MonoBehaviour
+{
+	private NavMeshAgent agent;
+	public Transform Destination;
+	
+	// Use this for initialization
+	void Start ()
+	{
+		agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Destination != null) agent.destination = Destination.position;
+	}
+}

1e53aa7d3e5edb6ca436d6cce23a1bce12b2dfe3
Colin Richins <zeldianiac@gmail.com>
2018-11-07 15:26:07 -0700

    While Loops
    Whoopdy Loopdy Doo we do some loops for you!

---
+++ 1610 Fall 2018/Assets/Scripts/EventRepeat.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class EventRepeat : MonoBehaviour
+{
+
+	public UnityEvent[] OnRepeat;
+	public float WaitForSeconds = 2;
+	private int i = 0;
+
+	IEnumerator Start()
+	{
+		while (true)
+		{
+			yield return new WaitForSeconds(WaitForSeconds);
+			OnRepeat[i].Invoke();
+			if (i < OnRepeat.Length-1)
+			{
+				i++;
+			}
+			else
+			{
+				i = 0;
+			}
+		}
+	}
+}
